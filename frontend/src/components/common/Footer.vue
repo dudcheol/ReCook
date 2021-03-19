b89065ac053653702bf86bfa2675e042779b7c48
dudcheol <95_pyc@naver.com>
2021-03-19 11:29:56 +0900

    feat: 🎸 setting - [FE] 헤더,푸터 틀
    ✅ Closes: S04P22A204-100

---
+++ frontend/src/components/common/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-bottom-navigation v-model="value" grow absolute flat>
+    <v-btn class="bottom-btn" value="recent">
+      <span>Home</span>
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+
+    <v-btn class="bottom-btn" value="favorites">
+      <span>Feed</span>
+      <v-icon>mdi-view-dashboard</v-icon>
+    </v-btn>
+
+    <v-btn class="bottom-btn" value="nearby" x-large>
+      <v-icon large>mdi-fridge-outline</v-icon>
+    </v-btn>
+
+    <v-btn class="bottom-btn" value="nearby">
+      <span>Watch</span>
+      <v-icon>mdi-play-circle</v-icon>
+    </v-btn>
+
+    <v-btn class="bottom-btn" value="nearby">
+      <span>Mypage</span>
+      <v-icon>mdi-account</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {},
+  data() {
+    return {
+      value: 'recent',
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.v-btn.bottom-btn {
+  min-width: 20px !important;
+  font-size: 1px;
+}
+</style>
