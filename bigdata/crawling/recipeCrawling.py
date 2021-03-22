ff69ae30f7665e25619a44d9b2baa653524e8709
박유정 <1693013@naver.com>
2021-03-22 17:05:37 +0900

    feat: 🎸 데이터 롤링
    데이터 크롤링
    
    ✅ Closes: S04P23A204-16

---
+++ bigdata/crawling/recipeCrawling.py
@@ -0,0 +1,66 @@
+import requests
+from bs4 import BeautifulSoup
+
+baseUrl = 'http://www.10000recipe.com/recipe/'
+
+# def CrawlingBetweenRanges(mydb, startRecipeId, endRecipeId):
+#     for i in range(startRecipeId, endRecipeId):
+#         if i % 10 == 0:
+#             print("count: " + str(i))
+#         res = PageCrawler(str(i))
+#         if res is None:
+#             continue
+
+#         menuId = mydb.insert_menu(res[0][0], baseUrl+str(i))
+#         for key, value in res[1].items():
+#             for name in value:
+#                 if key == "[재료]" or key == "[양념]":
+#                     mydb.insert_ingredient(menuId, name)
+
+def PageCrawler(recipeUrl):
+    url = baseUrl + recipeUrl
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    recipe_title = []  # 레시피 제목
+    recipe_source = {}  # 레시피 재료
+    # recipe_step = [] #레시피 순서
+
+    try:
+        res = soup.find('div', 'view2_summary')
+        res = res.find('h3')
+        recipe_title.append(res.get_text())
+        res = soup.find('div', 'view2_summary_info')
+        recipe_title.append(res.get_text().replace('\n', ''))
+        res = soup.find('div', 'ready_ingre3')
+    except(AttributeError):
+        return
+
+    # 재료 찾는 for문 가끔 형식에 맞지 않는 레시피들이 있어 try/ except 해준다
+    try:
+        for n in res.find_all('ul'):
+            source = []
+            title = n.find('b').get_text()
+            recipe_source[title] = ''
+            for tmp in n.find_all('li'):
+                tempSource = tmp.get_text().replace('\n', '').replace(' ', ' ')
+                source.append(tempSource.split("    ")[0])
+
+            recipe_source[title] = source
+    except (AttributeError):
+        return
+
+    recipe_all = [recipe_title, recipe_source]  #제목, 재료
+
+    print(recipe_title)
+    for key, value in recipe_all[1].items():
+        for name in value:
+            if key == "[재료]" or key == "[양념]":
+                print(name)
+                
+    return (recipe_all)
+
+    
+if __name__ == "__main__":
+    PageCrawler('6894096')
