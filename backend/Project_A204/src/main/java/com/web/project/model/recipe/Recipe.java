e1d2ac3b5d10411ff0094fc571c8b7f38a0eba65
wjd1rb <wjd1rb@gmail.com>
2021-03-24 11:12:15 +0900

    feat: 🎸 recipe - [BE] recipe model 생성]
    recipe model 생성

---
+++ backend/Project_A204/src/main/java/com/web/project/model/recipe/Recipe.java
@@ -0,0 +1,36 @@
+package com.web.project.model.recipe;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Recipe {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int recipeId;
+	
+	private String recipeTitle;
+	
+	@Column(insertable = false, updatable = false)
+	private LocalDateTime recipeCreated;
+	
+	private String recipeImage;
+	
+	private String recipeContext;
+	
+	private String recipeIngredient;
+	
+	private String recipeTime;
+}
