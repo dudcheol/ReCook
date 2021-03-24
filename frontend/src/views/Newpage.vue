e1bda275bd34109e014d6cfc6e8c26c5b698ec4b
dudcheol <95_pyc@naver.com>
2021-03-24 14:25:26 +0900

    feat: 🎸 review - [FE] 리뷰페이지 틀 제작 및 헤더 연결 수정
    인피니티 그리드 레이아웃 적용
    
    ✅ Closes: S04P23A204-26

--- frontend/src/views/Newpage.vue
+++ frontend/src/views/Newpage.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <Header :leftBtn="left" :title="center" :rightBtn="right" />
+    <Header />
     <router-view></router-view>
   </div>
 </template>
@@ -20,39 +20,7 @@ export default {
     };
   },
   computed: {},
-  watch: {
-    '$route.params.type': {
-      immediate: true,
-      handler(type) {
-        switch (type) {
-          case 'fridge-ingredient':
-            this.left = 'close';
-            this.center = '남은 재료 선택';
-            this.right = '';
-            break;
-          case 'fridge-recomm':
-            this.left = 'close';
-            this.center = '남은 재료로 추천받기';
-            this.right = '';
-            break;
-          case 'recipe-detail':
-            this.left = 'chevron-left';
-            this.center = '';
-            this.right = 'heart';
-            break;
-          case 'review-detail':
-            this.left = 'close';
-            this.center = '';
-            this.right = '';
-            break;
-          case 'profile-edit':
-            this.left = 'close';
-            this.center = '프로필 수정';
-            this.right = '';
-        }
-      },
-    },
-  },
+  watch: {},
   methods: {},
 };
 </script>
