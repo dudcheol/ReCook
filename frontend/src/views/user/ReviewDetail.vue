a0ca8a037c5ecba669316cf228344f40c95f3a2e
dudcheol <95_pyc@naver.com>
2021-03-25 11:21:24 +0900

    feat: 🎸 review - [FE] 리뷰 상세 페이지 틀 제작
    리뷰 상세페이지 틀 제작
    
    ✅ Closes: S04P23A204-33

--- frontend/src/views/user/ReviewDetail.vue
+++ frontend/src/views/user/ReviewDetail.vue
@@ -1,11 +1,50 @@
 <template>
-  <div>
-    <h1>ReviewDetail {{ $route.params.id }}</h1>
-  </div>
+  <v-container>
+    <v-row>
+      <v-col>
+        <ProfileSimpleItem :username="'test'" />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col class="pa-0">
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
+          max-height="500"
+          aspect-ratio="1"
+        >
+        </v-img>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <RecipeSimpleItem :title="'레시피 이름'" :rating="4.5" />
+      </v-col>
+    </v-row>
+    <v-divider class="my-3"></v-divider>
+    <v-row>
+      <v-col class="text-body-1 font-weight-regular">
+        {{ content }}
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-export default {};
+import ProfileSimpleItem from '@/components/ProfileSimpleItem';
+import RecipeSimpleItem from '@/components/RecipeSimpleItem.vue';
+
+export default {
+  components: { ProfileSimpleItem, RecipeSimpleItem },
+  props: {},
+  data() {
+    return {
+      content: '리뷰 테스트 화면입니다.',
+    };
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
 </script>
 
 <style scoped></style>
