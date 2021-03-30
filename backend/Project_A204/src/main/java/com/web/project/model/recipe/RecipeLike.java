76c62c2b9690ef5c558ddfa5685359597fcaf547
wjd1rb <wjd1rb@gmail.com>
2021-03-30 14:39:23 +0900

    feat: 🎸 like - [BE] 레시피 좋아요 기능 구현 완료
    레시피 좋아요 기능 구현 완료
    
    ✅ Closes: S04P23A204-81

--- backend/Project_A204/src/main/java/com/web/project/model/recipe/RecipeLike.java
+++ backend/Project_A204/src/main/java/com/web/project/model/recipe/RecipeLike.java
@@ -1,14 +1,20 @@
 package com.web.project.model.recipe;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import lombok.Data;
 
+@Entity
 @Data
 public class RecipeLike {
-	
-	private boolean isLike;
-	
-	private int recipeId;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int likeId;
 	
 	private String userId;
 	
+	private int recipeId;
 }
