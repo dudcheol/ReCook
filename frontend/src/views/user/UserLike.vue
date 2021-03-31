e3b486e313f1c748235e7351cbd67411ac40c334
dudcheol <95_pyc@naver.com>
2021-03-31 18:16:50 +0900

    feat: 🎸 user - [FE] 무한스크롤 등록

--- frontend/src/views/user/UserLike.vue
+++ frontend/src/views/user/UserLike.vue
@@ -3,14 +3,16 @@
     <v-row class="header-profile white pt-16">
       <v-col>
         <profile-simple-item
-          :username="'test'"
-          :src="'https://picsum.photos/500/300?image=1'"
+          :username="user.userName || ''"
+          :src="user.userImage || ''"
           class="px-3"
         ></profile-simple-item>
       </v-col>
     </v-row>
     <v-row class="pt-14 pb-4" no-gutters>
-      <v-col> </v-col>
+      <v-col>
+        <infinite-loading @infinite="infiniteHandler"></infinite-loading>
+      </v-col>
     </v-row>
   </v-container>
 </template>
@@ -24,11 +26,33 @@ export default {
     user: Object,
   },
   data() {
-    return {};
+    return {
+      page: 1,
+      list: [],
+    };
   },
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    infiniteHandler($state) {
+      console.log('%cUserLike.vue line:38 $state', 'color: #007acc;', $state);
+      // axios
+      //   .get(api, {
+      //     params: {
+      //       page: this.page,
+      //     },
+      //   })
+      //   .then(({ data }) => {
+      //     if (data.hits.length) {
+      //       this.page += 1;
+      //       this.list.push(...data.hits);
+      //       $state.loaded();
+      //     } else {
+      //       $state.complete();
+      //     }
+      //   });
+    },
+  },
 };
 </script>
 <style scoped>
