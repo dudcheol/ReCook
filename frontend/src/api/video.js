8c9da880962bccb68b4787f93be1b0f0d5bab74d
wjd1rb <wjd1rb@gmail.com>
2021-04-02 16:59:11 +0900

    feat: 🎸 video - [FE] video 출력
    youtube video 링크를 통한 출력
    
    ✅ Closes: S04P23A204-80

--- frontend/src/api/video.js
+++ frontend/src/api/video.js
@@ -4,5 +4,11 @@ const instance = createInstance();
 const COMMON = 'video';
 
 // 여기에 코드 작성
+function getVideoById(video_id, success, fail) {
+    instance
+      .get(`${COMMON}/detail/${video_id}`)
+      .then(success)
+      .catch(fail);
+  }
 
-export {};
+export { getVideoById };
--- frontend/src/views/watch/WatchDetail.vue
+++ frontend/src/views/watch/WatchDetail.vue
@@ -1,21 +1,51 @@
 <template>
   <div>
-    <h1>Watch detail vue</h1>
+    <h2>{{ videoInfo.videoTitle }}</h2>
+
+    <v-img
+      min-height="200"
+      max-height="200"
+      :src="videoInfo.videoThumbnail"
+      class="rounded-lg"
+    >
+    </v-img>
+
+    <youtube 
+      :video-id=getVideoId(videoInfo.videoUrl) 
+      player-width="375" 
+      player-height="200"
+    >
+    </youtube>
+
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex';
+import Vue from 'vue';
+import VueYouTubeEmbed from 'vue-youtube-embed';
+// import { getIdFromURL } from 'vue-youtube-embed';
+ 
+Vue.use(VueYouTubeEmbed)
+
 export default {
   components: {},
   props: {},
   data() {
-    return {};
+    return {
+      videoId: '',
+    };
   },
-  computed: {},
+  computed: {
+    ...mapState({
+      videoInfo: (state) => state.video.videoInfo,
+    }),
+  },
   watch: {
     $route: {
       immediate: true,
       handler(value) {
+        this.addVideoById(value.params['video_id']);
         console.log(
           '%cWatchDetail.vue line:19 value.params',
           'color: #007acc;',
@@ -24,7 +54,23 @@ export default {
       },
     },
   },
-  methods: {},
+  methods: {
+    ...mapActions(['addVideoById']),
+    getVideoId (url) {
+      this.videoId = this.$youtube.getIdFromURL(url)
+    },
+    ready (event) {
+      this.player = event.target
+    },
+    playing () {
+    },
+    stop () {
+      this.player.stopVideo()
+    },
+    pause () {
+      this.player.pauseVideo()
+    }
+  },
 };
 </script>
 
