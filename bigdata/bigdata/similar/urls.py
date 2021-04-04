3dc3e41c02b6f6900e9b6564016f9491ef508b23
dooly7183 <dooly7183@naver.com>
2021-04-04 22:42:48 +0900

    feat: 🎸 urls 주석 변경
    urls 주석 변경

--- bigdata/bigdata/similar/urls.py
+++ bigdata/bigdata/similar/urls.py
@@ -6,5 +6,5 @@ app_name="views"
 urlpatterns = [
     path('recipe/<title>/<userid>', views.index), #연관 레시피
     path('recommend/<user_id>', views.choice), #추천 레시피(리뷰 3개 이하면 취향조사 반영, 리뷰 3개이상이면 협업필터링)
-    path('main/<title>/<user_id>', views.main_index),
+    path('main/<title>/<user_id>', views.main_index),#메인 레시피
 ]
