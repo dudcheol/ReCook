d3b662b81166c820b2b5a38f444ffd7166fba32e
dudcheol <95_pyc@naver.com>
2021-04-07 15:34:46 +0900

    fix: 🐛 main - [FE] 레시피 리스트 전체보기 페이지 디자인 피드백
    ✅ Closes: S04P23A204-167

--- frontend/src/components/RecipeRecommCardItem.vue
+++ frontend/src/components/RecipeRecommCardItem.vue
@@ -1,12 +1,12 @@
 <template>
   <v-card class="pa-2" rounded="xl" elevation="0" @click="$emit('click')">
     <div class="d-flex flex-no-wrap">
-      <v-avatar class="ma-3" size="125">
+      <v-avatar class="ma-3" size="120">
         <v-img :src="data.recipeMainImage || data['recipe-main-image']"></v-img>
       </v-avatar>
       <div class="pa-3 my-auto">
-        <div class="headline pt-2">
-          {{ (data.recipeTitle || data['recipe-title']) | truncate(14, '..') }}
+        <div class="h6 pt-2">
+          {{ (data.recipeTitle || data['recipe-title']) | truncate(9, '..') }}
         </div>
 
         <div class="caption-1 pt-2">
